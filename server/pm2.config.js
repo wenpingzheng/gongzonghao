3299ba3cab0f1e68387994a20d2ab3d1aa93f295
wp <zhengwenping@wps-MacBook-Pro.local>
2023-08-20 21:20:11 +0800

    feat: update readme
    --story=1231332

--- server/pm2.config.js
+++ server/pm2.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: 'gzh',
       script: './dist/app.js',
       watch: true,
-      ignore_watch: ['node_modules'],
+      ignore_watch: ['node_modules', 'logs'],
       cwd: './',
       env: {
         NODE_ENV: 'production',
