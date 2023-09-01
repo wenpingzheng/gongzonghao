1c69e85022b8d186bb40c71d0247e973aeba6b4e
wp <zhengwenping@wps-MacBook-Pro.local>
2023-09-01 23:31:34 +0800

    feat: update readme
    --story=1231332

---
+++ server/src/controller/html/git-learn.js
@@ -0,0 +1,11 @@
+import ejs from 'ejs';
+import path from 'path';
+
+export default {
+  async show(ctx) {
+    const html = await ejs.renderFile(path.join(__dirname, '../../../views/git-learn.ejs'), {
+      title: '首页'
+    });
+    ctx.body = html;
+  }
+}
--- server/src/router/index.js
+++ server/src/router/index.js
@@ -7,12 +7,16 @@ import wx from '../controller/apis/wx';
 // HTML
 import index from '../controller/html/index';
 import home from '../controller/html/home';
+import gitLearn from '../controller/html/git-learn';
 
 const router = new Router();
 
 // 路由 - 页面
 router.get('/', index.show);
 router.get('/home', home.show)
+
+// 课程页 - GIT开发
+router.get('/page/git-learn/1', gitLearn.show)
 
 // 路由 - 接口
 router.get('/api/gettoken', token.getToken);
